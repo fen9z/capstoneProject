347ef9d72c3c91fe80f5725aaafa0d3adb4255b2
zhoufengfun <zhoufengfun@outlook.com>
2023-10-29 21:49:39 -0400

    create auth function in server, basic structure

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+// load user model
+const User = require('../models/userModel')
+
+// login user
+const loginUser = async (req, res) => {
+  res.json({mssg: 'login user'})
+}
+
+// signup user
+const signupUser = async  (req, res) => {
+  res.json({mssg: 'signup user'})
+}
+
+module.exports = {
+  loginUser,
+  signupUser,
+}
