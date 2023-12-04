5ca21a81b88be12f52ec00b59e3b710c77d73e94
tijo <ttomy5175@conestogac.on.ca>
2023-12-04 18:11:55 -0500

    home page design

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,11 +1,31 @@
 import { Link } from 'react-router-dom';
 import book from '../Asset/boook.jpg';
+import hold from '../Asset/hold.jpg';
+import chat from '../Asset/chat.jpg';
+
 import '../style/styles.css';
 const Card = ({ card }) => {
   return (
     <Link to={'/' + card}>
       <div className="card">
-        <img src={book} alt="book" style={{ width: '100%', height: '60%' }} />
+        {
+          card=="Booking" &&(
+            <img src={book} alt="book" style={{ width: '100%', height: '60%' }} />
+          )
+          
+        }
+        {
+          card=="Hold" && (
+            <img src={hold} alt="hold" style={{ width: '100%', height: '60%' }} />
+
+          )
+        }
+        {
+          card=="Chat" && (
+            <img src={chat} alt="hold" style={{ width: '100%', height: '60%' }} />
+
+          )
+        }
         <button type="button" className=" mt-2 btns">
           {card}
         </button>
